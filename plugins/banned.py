9a914323b35d9eed09c2ed046b0a04f2f4404000
UNKOWN5757H <65911268+UNKOWN5757H@users.noreply.github.com>
2026-03-10 12:16:40 +0530

    Update owner button URL in banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -26,7 +26,7 @@ async def ban_reply(bot, message):
 @Client.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
     buttons = [[
-        InlineKeyboardButton('𝙾𝚆𝙽𝙴𝚁', url=f'https://t.me/EnthadaNokunne')
+        InlineKeyboardButton('𝙾𝚆𝙽𝙴𝚁', url=f'https://t.me/Nikhil5757h')
     ]]
     reply_markup=InlineKeyboardMarkup(buttons)
     vazha = await db.get_chat(message.chat.id)
