911f5a1906c056ed30d3f27b29f8a41ccdc53f26
UNKOWN5757H <65911268+UNKOWN5757H@users.noreply.github.com>
2026-03-10 15:19:10 +0530

    Fix formatting of text in pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -416,7 +416,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
+            text=script.START_TXT.format(query.from_user.mention, uname=temp.U_NAME, bname=temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
