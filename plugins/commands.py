b84be468f837e15c1e7270976df2cc6f6e4a6e9f
UNKOWN5757H <65911268+UNKOWN5757H@users.noreply.github.com>
2026-07-24 08:00:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -40,9 +40,8 @@ DELETE_TIME = 1800  # 30 Minutes in seconds
 LOG_FILE = "TelegramBot.log"
 
 # FIXED: Replaced single quotes with double quotes for the emoji-id attribute.
-MESSAGE_EMOJI_PLANE = '<tg-emoji emoji-id="5258073068852485953">✈️</tg-emoji>'
-MESSAGE_EMOJI_LINK = '<tg-emoji emoji-id="5260730055880876557">🔗</tg-emoji>'
-
+MESSAGE_EMOJI_PLANE = "<emoji id=5258073068852485953>✈️</emoji>"
+MESSAGE_EMOJI_LINK = "<emoji id=5260730055880876557>🔗</emoji>"
 
 def get_start_buttons(user_id):
     """Helper to generate start buttons dynamically based on admin status."""
@@ -92,6 +91,8 @@ async def start(client: Client, message: Message):
                 ),
                 uname=temp.U_NAME,
                 bname=temp.B_NAME,
+                plane_emoji=MESSAGE_EMOJI_PLANE, 
+                link_emoji=MESSAGE_EMOJI_LINK,
             ),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML,  # FIXED: Added missing parse_mode to render emojis
