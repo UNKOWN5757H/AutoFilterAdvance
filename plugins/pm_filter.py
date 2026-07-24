50e10edcf727faa57106e7d93362cef7efba1e26
UNKOWN5757H <65911268+UNKOWN5757H@users.noreply.github.com>
2026-07-24 07:56:35 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -60,9 +60,8 @@ NOT_FOUND_TEXT = (
 
 # FIXED: Replaced single quotes with double quotes for the emoji-id attribute.
 # Telegram HTML parser requires double quotes to render custom emojis.
-MESSAGE_EMOJI_PLANE = '<tg-emoji emoji-id="5258073068852485953">✈️</tg-emoji>'
-MESSAGE_EMOJI_LINK = '<tg-emoji emoji-id="5260730055880876557">🔗</tg-emoji>'
-
+MESSAGE_EMOJI_PLANE = "<emoji id=5258073068852485953>✈️</emoji>"
+MESSAGE_EMOJI_LINK = "<emoji id=5260730055880876557>🔗</emoji>"
 
 async def delete_message_after_delay(message, delay: int):
     if not message:
