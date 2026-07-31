b0b21f29eba44fc702b149af7a74c6d9bbe7b8bb
UNKOWN5757H <65911268+UNKOWN5757H@users.noreply.github.com>
2026-07-31 13:16:50 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 class script(object):
     START_TXT = """<b>Hey {mention} bro 🥂,\nI'm {bname}, the Moviebot! \n \nJoin our gang — We've got Movies hotter than your ex's new filing 😊🎬</b>"""
-    HELP_TXT = """<b>You can find the bot commands here.\n \n‣ /help - Show this help message\n \n<quote>Tap on corresponding modules to get the commands of that module.</quote></b>"""
+    HELP_TXT = """<b>You can find the bot commands here.\n \n‣ /help - Show this help message\n \n<blockquote>Tap on corresponding modules to get the commands of that module.</blockquote></b>"""
     ABOUT_TXT = """<b>○ 𝗠𝘆 𝗡𝗮𝗺𝗲 : {bname}
 ○ 𝗖𝗿𝗲𝗮𝘁𝗼𝗿 : <a href=https://t.me/SandalwoodSupportBot><b>Sandalwood Support</b></a>
 ○ 𝗟𝗶𝗯𝗿𝗮𝗿𝘆 : 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
@@ -8,6 +8,61 @@ class script(object):
 ○ 𝗗𝗮𝘁𝗮𝗕𝗮𝘀𝗲 : 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ○ 𝗕𝘂𝗶𝗹𝗱 𝗩𝗲𝗿𝘀𝗶𝗼𝗻 : V2.0 [ 𝙱𝙴𝚃𝙰 ]</b>"""
     SOURCE_TXT = """<b>Source Code Of This Bot is Private 😊"""
+    BANS_TXT = """<b><blockquote>User Management\nBan or unban users to control access to the bot.</blockquote>\n \n‣ /ban - Ban a user from bot - /ban user_id\n‣ /unban - Unban a user from bot - /unban user_id</b>"""
+    CUSTOMMESSAGE_TXT = """<b><blockquote>Custom Messages & Images\nConfigure custom messages and images for various actions, such as file info, file deletion, file not found, or force subscription prompts.</blockquote>\n \n‣ /infomsg - Set info message before sending file - Reply /infomsg to a message to set or  /infomsg off to remove\n
+‣ /infoimg - Set info image before sending file - Reply /infoimg to an image to set or  /infoimg off to remove\n
+‣ /delmsg - Set delete message after sending file (File auto delete needs to be enabled to work) - Reply /delmsg to a message to set or  /delmsg off to remove\n
+‣ /delimg - Set delete image after sending file (File auto delete needs to be enabled to work) - Reply /delimg to an image to set or  /delimg off to remove\n
+‣ /notfoundmsg - Set message to send when file not found - Reply /notfoundmsg to a message to set or  /notfoundmsg off to remove\n
+‣ /notfoundimg - Set image to send when file not found - Reply /notfoundimg to an image to set or  /notfoundimg off to remove\n
+‣ /fsubmsg - Set force subscribe message - Reply /fsubmsg to a message to set or  /fsubmsg off to remove\n
+‣ /fsubimg - Set force subscribe image - Reply /fsubimg to an image to set or  /fsubimg off to remove</b>"""
+    CUSTOMCAPTIONS = """<b><blockquote>File Caption Management\nManage or customize captions for files, including additional captions, to enhance file presentation.</blockquote>\n \n‣ /customcaption - Set custom caption for files - Reply /customcaption to a message to set or /customcaption off to disable.\n
+‣ /captionplus - Set additional caption for files along with caption - Reply /captionplus to a message to set or /captionplus off to disable.</b>"""
+    DELETE_TXT = """<b><blockquote>File/Auto Deletion Management\nDelete files from the database or configure auto-delete settings for files and button messages in groups.</blockquote>\n \n‣ /delete - Reply to a file to delete it from database\n
+‣ /delmulti - Delete multiple files from database with name - /delmulti name\n
+‣ /autodelete - Set file auto delete time in seconds\n
+‣ /buttondel - Set button message in groups auto delete time in seconds</b>"""
+    FORCESUB_TXT = """<b><blockquote>Force Subscription Management\nSet, manage, or clear force subscribe channels.</blockquote>\n \n‣ /setfsubcount - Set maximum Fsub chat count for queue (If not set, queued FSub will be activated instantly)\n
+‣ /setfsub - Set force subscribe channel - /setfsub channel_id (Answer the questions after that) Bot must be admin of that channel (Bot will create a new invite link)\n
+‣ /rmfsub - Remove force subscribe channel - /rmfsub channel_id\n
+‣ /rmallfsub - Remove all force subscribe channels\n
+‣ /getallfsub - Get all force subscribe channel details\n
+‣ /getactivefsub - Get active force subscribe channels\n
+‣ /getpendingfsub - Get pending force subscribe channels which is in queue\n
+‣ /activatefsub - Activate pending force subscribe channel - /activatefsub channel_id\n
+‣ /deactivatefsub - Deactivate force subscribe channel - /deactivatefsub channel_id\n
+‣ /updatefsubtarget - Update force subscribe channel target - /updatefsubtarget channel_id target\n
+‣ /checkfsubusers - Check force subscribe users count\n
+‣ /clearfsubusers - Clear all force subscribe users from db</b>"""
+    FILTERS_TXT = """<b><blockquote>Filter Management\nAdd, delete, or view filters to customize responses based on keywords.</blockquote>\n \n‣ /filter - Add a text filter - Reply /filter keyword to a message to set (Supports custom button formats).\n
+‣ /addfilter - (Only for messages with pre made buttons)Add a text filter - Reply /addfilter keyword to a message to set (If a filter is there, bot will send the filter rather than file)\n
+‣ /delfilter - Delete a text filter - /delfilter filter\n
+‣ /listfilters - List all filters currently added in the bot\n \nSupports text/photo/video/animation/sticker</b>"""
+    INDEX_TXT = """<b><blockquote>Database Indexing\nIndex a database channel by forwarding messages or using links. Supports private channels if the bot is an admin.</blockquote>\n \n‣ /index - Start indexing a database channel (bot must be admin of the channel if that is private channel)\n
+You can just forward the message from database channel for starting indexing, no need to use the /index command\n
+‣ /indexlink - Start indexing a database channel using link (bot must be admin of the channel if that is private channel)\n
+/indexlink last_message_link or /indexlink start_message_link last_message_link</b>"""
+    PROMOTIONS_TXT = """<b><blockquote>Manage Promotional Links\nEasily add, delete, or view promotional links displayed between search results.</blockquote>\n \n‣ /addpromo - Set promotional links between results - /addpromo "Button Text" URL\n
+‣ /delpromo - Delete promotional links between results - /delpromo URL\n
+‣ /listpromos - List all promotional links currently added in the DB</b>"""
+    SETTINGS_TXT = """<b><blockquote>Bot Settings Management</blockquote>\n \n‣ /repairmode - Enable or disable repair mode - If on, bot will not send any files\n
+‣ /adminsettings - Get current admin settings</b>"""
+    UTILITIES_TXT = """<b><blockquote>Utility Commands\nAccess bot logs, server stats, restart the bot, get user and file counts, send broadcasts, and more.</blockquote>\n \n‣ /logs - Get logs as a file\n
+‣ /server - Get server stats\n
+‣ /restart - Restart the bot\n
+‣ /stats - Get bot user stats (Will send only after checking active users\n
+‣ /broadcast - Reply to a message to send that to all bot users\n
+‣ /total - Get count of total files in DB\n
+‣ /clearfiles - Clear all files from DB\n
+‣ /clearusers - Clear all users from DB\n
+‣ /clearfsubusers - Clear all force subscribe users from db</b>"""
+    BACKUP_TXT = """<b><blockquote>Database Backup Management\nManage database backups, including scheduled backups and manual backups.</blockquote>\n \nThese commands are only available for the owner of the bot.\n
+‣ /dbbackup - Backup the database\j
+‣ /dbrestore - Restore the database\n
+‣ /dbstats - Get database stats\n
+‣ /dbschedule - Get scheduled backup status</b>"""
+    
     MANUELFILTER_TXT = """𝗙𝗶𝗹𝘁𝗲𝗿𝘀
 
 - Filter is the feature were users can set automated replies for a particular keyword and I will respond whenever a keyword is found the message
