784167739d56aabb4e2bc54ce4881dfa9afa9059
UNKOWN5757H <65911268+UNKOWN5757H@users.noreply.github.com>
2026-07-31 13:47:43 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -14,7 +14,7 @@ class script(object):
 
     BANS_TXT = """<b><blockquote>User Management\nBan or unban users to control access to the bot.</blockquote>\n \n‣ /ban - Ban a user from bot - /ban user_id\n‣ /unban - Unban a user from bot - /unban user_id</b>"""
 
-    CUSTOMMESSAGE_TXT = """<b><blockquote>Custom Messages & Images\nConfigure custom messages and images for various actions, such as file info, file deletion, file not found, or force subscription prompts.</blockquote>\n
+    CUSTOMMESSAGES_TXT = """<b><blockquote>Custom Messages & Images\nConfigure custom messages and images for various actions, such as file info, file deletion, file not found, or force subscription prompts.</blockquote>\n
 ‣ /infomsg - Set info message before sending file - Reply /infomsg to a message to set or /infomsg off to remove
 ‣ /infoimg - Set info image before sending file - Reply /infoimg to an image to set or /infoimg off to remove
 ‣ /delmsg - Set delete message after sending file (File auto delete needs to be enabled to work) - Reply /delmsg to a message to set or /delmsg off to remove
@@ -24,7 +24,7 @@ class script(object):
 ‣ /fsubmsg - Set force subscribe message - Reply /fsubmsg to a message to set or /fsubmsg off to remove
 ‣ /fsubimg - Set force subscribe image - Reply /fsubimg to an image to set or /fsubimg off to remove</b>"""
 
-    CUSTOMCAPTIONS = """<b><blockquote>File Caption Management\nManage or customize captions for files, including additional captions, to enhance file presentation.</blockquote>\n
+    CUSTOMCAPTION_TXT = """<b><blockquote>File Caption Management\nManage or customize captions for files, including additional captions, to enhance file presentation.</blockquote>\n
 ‣ /customcaption - Set custom caption for files - Reply /customcaption to a message to set or /customcaption off to disable.
 ‣ /captionplus - Set additional caption for files along with caption - Reply /captionplus to a message to set or /captionplus off to disable.</b>"""
 
@@ -79,6 +79,23 @@ You can just forward the message from database channel for starting indexing, no
 ‣ /clearfiles - Clear all files from DB
 ‣ /clearusers - Clear all users from DB
 ‣ /clearfsubusers - Clear all force subscribe users from db</b>"""
+
+    CONNECTIONS_TXT = """<b><blockquote>𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀 𝗮𝗻𝗱 𝗨𝘀𝗮𝗴𝗲\n - Used to connect bot to PM for managing filters\n- it helps to avoid spamming in groups.</blockquote>\n
+‣ /connect  - Connect a particular chat to your PM
+‣ /disconnect  - Disconnect from a chat
+‣ /connections - List all your connections</b>"""
+
+    FORCEADD_TXT = """<b><blockquote>ForceAdd Management</blockquote>\n
+
+‣ /setforceadd - Set a Force Add channel for the group.
+‣ /remforceadd - Remove the current Force Add channel.
+‣ /getforceadd - View the configured Force Add channel.
+‣ /topaddall - Show the all-time top inviters leaderboard.
+‣ /topadd24 - Show the top inviters in the last 24 hours.
+‣ /topadd7 - Show the top inviters from the last 7 days.
+‣ /resetadddaily - Reset today's add statistics.
+‣ /resetadd - Reset all Force Add statistics.
+‣ /myadds - Check your personal add count and ranking.</b>"""
 
     BACKUP_TXT = """<b><blockquote>Database Backup Management\nManage database backups, including scheduled backups and manual backups.</blockquote>\n \nThese commands are only available for the owner of the bot.\n
 ‣ /dbbackup - Backup the database
