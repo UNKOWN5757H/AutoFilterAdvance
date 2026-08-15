2d13f05dcfe6f8cc80276e8231277546f55f40d4
UNKOWN5757H <65911268+UNKOWN5757H@users.noreply.github.com>
2026-08-15 21:03:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1169,7 +1169,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             k = await client.send_message(
                 chat_id=query.from_user.id,
                 text=(
-                    "<b>📢 Pʟᴇᴀꜱᴇ Nᴏᴛᴇ\n\n✅ Tʜᴇ Aʙᴏᴠᴇ Fɪʟᴇ Wɪʟʟ Bᴇ Aᴜᴛᴏᴅᴇʟᴇᴛᴇᴅ Iɴ 30ᴍɪɴꜱ Tᴏ Aᴠᴏɪᴅ Cᴏᴘʏʀɪɢʜᴛ Iꜱꜱᴜᴇꜱ.\n\nTᴇᴀᴍ: @KR_Picture</b>"
+                    "<b>📢 <u>Please Note</u>\n \n✅ The above file will be autodeleted in 30mins to avoid copyright issues.\n \n✅ Please forward this file to your saved messages and start downloading from there.\n \nTᴇᴀᴍ: @KR_Picture</b>"
                 ),
             )
 
