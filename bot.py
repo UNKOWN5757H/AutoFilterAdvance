1e0ed410b332ac6fa820f4ff76c30b24b2050aa6
UNKOWN5757H <65911268+UNKOWN5757H@users.noreply.github.com>
2026-09-23 08:31:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -250,4 +250,3 @@ if __name__ == "__main__":
         loop.run_until_complete(start_services())
     except (KeyboardInterrupt, SystemExit):
         logger.info("Process interrupted. Shutting down...")
-```eof
